7f5a360edc46de8dba0bc03f0f11f177189f9801
suaviludius <powerranger1912@gmail.com.com>
2026-07-02 23:55:19 +0300

    feat(backend):  созданы свойства статусов выполнения программы и прогресса; добавлены слоты управления режимами работы; добавлены сигналы обновления прогресса, логов и статуса работы

--- include/FMBackend.h
+++ include/FMBackend.h
@@ -2,12 +2,117 @@
 #define FMBACKEND_H
 
 #include <QObject>
+#include <QTimer>
+#include <QThreadPool>
 
+#include <atomic>
+#include <queue>
+
+// Структуа конфигурации с фронта
+struct Config {
+    QString path;
+    QString mask;
+    QString outputPath;
+    QString xorValue;
+    bool deleteOriginal = false;
+    bool useTimer = false;
+    int intervalSec = 0;
+};
+
+// Рабоиче состояния
+enum class Status{
+    READY,
+    RUNNING,
+    PAUSE,
+    STOPPED,
+    ERROR
+};
+
+// Главный класс, соединяющий qml и бекенд
 class FMBackend : public QObject {
     Q_OBJECT
+    Q_PROPERTY(bool isRunning READ isRunning NOTIFY statusChanged)
+    Q_PROPERTY(bool isPaused READ isPaused NOTIFY statusChanged)
+    Q_PROPERTY(QStringList logMessages READ logMessages NOTIFY logUpdated)
+    Q_PROPERTY(int progress READ progress NOTIFY progressChanged)
+    Q_PROPERTY(double speed READ speed NOTIFY progressChanged)
+    Q_PROPERTY(QString currentFile READ currentFile NOTIFY progressChanged)
 public:
     explicit FMBackend(QObject* parent = nullptr);
-    //~FMBackend() override;
+    ~FMBackend() override;
+
+    // Геттеры для свойств
+    bool isRunning() const {return m_running;}
+    bool isPaused() const {return m_paused;}
+    int progress() const {return m_currentProgress;}
+    double speed() const {return m_currentSpeed;}
+    QString currentFile() const {return m_currentFile;}
+    QStringList logMessages() const {return m_logMessages;}
+
+// Cлоты для подклюучения к сигналам qml элементов
+public slots:
+    // Валидация
+    bool checkPathExists(const QString& path);
+
+    // Управление
+    void startProcessing(
+        const QString& path,
+        const QString& mask,
+        const QString& otputPath,
+        const QString& xorValue,
+        bool deleteOriginal,
+        bool useTimer,
+        int intervalSec
+    );
+    void stopProcessing();
+    void pause();
+    void resume();
+
+// Сигналы для уведомлений
+signals:
+    // Изменение режима работы
+    void statusChanged();
+    // Обновление лог сообщений в ScrollView
+    void logUpdated();
+    // Обновление прогресса обработки
+    void progressChanged();
+
+private slots:
+    // Поиск файлов в дирректории по маске
+    void onTimerScan();
+
+private:
+    std::atomic<bool> m_running;
+    std::atomic<bool> m_paused;
+    std::atomic<bool> m_stopRequested;
+
+    // Очередь in файлов найденых по маске
+    std::queue<QString> m_fileQueue;
+    // Текущий in файл
+    QString m_currentFile;
+
+    // Параметры конфигурации для выполнения программы
+    std::unique_ptr<Config> m_config;
+
+    // Таймер
+    QTimer* m_timer;
+
+    // Прогресс обработки
+    int m_currentProgress = 0;
+    double m_currentSpeed = 0.0;
+
+    // Список с логами
+    QStringList m_logMessages;
+    // Максимальное количество логов в m_logMessages
+    static constexpr int MAX_LOG_SIZE = 100;
+
+    // Добавление лог сообщений
+    void addLog(const QString& msg);
+    // Обработка очереди
+    void processQueue();
+
 };
 
 #endif // FMBACKEND_H
+
+// D:/Projects/CPP/file_modifier/tests
--- src/FMBackend.cpp
+++ src/FMBackend.cpp
@@ -1,10 +1,180 @@
 #include "FMBackend.h"
 
+#include <QFile>
+#include <QFileInfo>
+#include <QDir>
+#include <QDebug>
+
+
 FMBackend::FMBackend(QObject* parent)
-    : QObject(parent){
+    : QObject(parent),  m_config(std::make_unique<Config>()), m_timer(new QTimer(this)){
     // initial
+    // Соединяем сигнал таймера со слотом периодической обработки файлов
+    connect(m_timer, &QTimer::timeout, this, &FMBackend::onTimerScan);
 }
 
-// FMBackend::~FMBackend(){
-//     // final
-// }
+FMBackend::~FMBackend() {
+    stopProcessing();
+}
+
+bool FMBackend::checkPathExists(const QString& path) {
+    if (path.isEmpty()) {
+        return false;
+    }
+
+    QDir dir(path);
+    return dir.exists();
+}
+
+void FMBackend::startProcessing(
+    const QString& path,
+    const QString& mask,
+    const QString& outputPath,
+    const QString& xorValue,
+    bool deleteOriginal,
+    bool useTimer,
+    int intervalSec) {
+
+    // Сохраняем конфигурацию
+    m_config->path = path;
+    m_config->mask = mask;
+    m_config->outputPath = outputPath;
+    m_config->xorValue = xorValue;
+    m_config->deleteOriginal = deleteOriginal;
+    m_config->useTimer = useTimer;
+    m_config->intervalSec = intervalSec;
+
+    // Валидация (уже проводится в qml)
+    // if (!validateConfig(m_config)) {
+    //     return;
+    // }
+
+    // ВЫводим сообщения в скрол консоль приложения
+    addLog("Запуск обработки...");
+    addLog("Поиск в: " + path);
+    addLog("Маска: " + mask);
+    addLog("Сохранение в: " + outputPath);
+
+    m_running = true;
+    m_paused = false;
+    m_stopRequested = false;
+
+    // Если таймер включен
+    if (m_config->useTimer) {
+        m_timer->start(m_config->intervalSec * 1000);
+        addLog("Режим таймера (интервал: " + QString::number(m_config->intervalSec) + "с )");
+    }
+
+    emit statusChanged();
+
+    // Начало обработки
+    onTimerScan();
+}
+
+void FMBackend::stopProcessing() {
+    m_stopRequested = true;
+    m_running = false;
+    m_paused = false;
+    m_timer->stop();
+
+    // Очищаем очередь
+    // TODO: заблокировать мьютекс!
+    while (!m_fileQueue.empty()) {
+        m_fileQueue.pop();
+    }
+
+    addLog("Обработка остановлена");
+    // TODO: придумать как фиксировать статус в qml
+    emit statusChanged();
+}
+
+
+void FMBackend::resume(){
+    if(m_running && m_paused){
+        addLog("Обработка приостановлена");
+        m_paused = false;
+        emit statusChanged();
+    }
+}
+
+void FMBackend::pause(){
+    if(m_running && !m_paused){
+        addLog("Обработка возобновлена");
+        m_paused = true;
+        emit statusChanged();
+    }
+}
+
+void FMBackend::onTimerScan() {
+    // Не в режиме остановки
+    if (!m_running || m_paused) return;
+
+    QDir dir(m_config->path);
+    QStringList filters;
+    filters << m_config->mask;
+
+    QStringList files = dir.entryList(filters, QDir::Files);
+
+    if (files.isEmpty()) {
+        addLog("Файлы не найдены по маске: " + m_config->mask);
+        return;
+    }
+
+    addLog("Найдено файлов: " + QString::number(files.size()));
+
+    for (const QString& file : files) {
+        if (m_stopRequested) {
+            break;
+        }
+        // TODO: заблокировать мьютекс!
+        m_fileQueue.push(file);
+    }
+
+    processQueue();
+}
+
+void FMBackend::processQueue() {
+    if (m_paused || m_stopRequested || !m_running) return;
+
+    // Запускаем обработку в отдельном потоке
+    while (!m_fileQueue.empty()) {
+        if (m_paused || m_stopRequested || !m_running) break;
+
+        std::string filePath;
+        {
+            if (m_fileQueue.empty()) break;
+            filePath = m_fileQueue.front().toStdString();
+            m_fileQueue.pop();
+        }
+
+        // Обработка файла
+        // здесь будет код обработки файла с XOR
+
+        // Пока что имитация прогресса для демонстрации
+        // m_currentProgress = 100;
+        // m_currentSpeed = 10;
+        // m_currentFile = QString::fromStdString(filePath);
+        // emit progressChanged();
+        for (int i = 3; i <= 100; i += 1) {
+            if (m_paused || m_stopRequested) break;
+            m_currentProgress = i;
+            m_currentSpeed =  (i / 100.0) * 50;
+            m_currentFile = QString::fromStdString(filePath);
+            emit progressChanged();
+        }
+
+        addLog("Обработан: " + QString::fromStdString(filePath));
+    }
+}
+
+void FMBackend::addLog(const QString& msg) {
+    m_logMessages.append(QDateTime::currentDateTime().toString("hh:mm:ss") + " " + msg);
+
+    if (m_logMessages.size() > MAX_LOG_SIZE) {
+        m_logMessages.removeFirst();
+    }
+
+    emit logUpdated();
+}
+
+
